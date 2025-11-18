5217893a70a8f5f7c79a5c56908557b325e7148b
araujoalca <andre_araujo@id.uff.br>
2025-11-18 12:23:23 -0300

    :sparkles: feat: montando a estrutura básica da página

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,14 +1,31 @@
+import BarraPesquisa from "@/src/widgets/BarraPesquisa";
+import BarraSuperior from "@/src/widgets/BarraSuperior";
+import Rodape from "@/src/widgets/Rodape";
+
 interface PageProps {
-    id: string
+  id: string
 }
 
 export default async function Page(
-    {params} : {params: Promise<PageProps>}
+  { params }: { params: Promise<PageProps> }
 ) {
-    const dados = await params
-    return (
-      <div>
-        url: {dados.id}
-      </div>
-    );
+  const dados = await params
+  return (
+    <div>
+
+      <header className="container mx-auto">
+        <BarraSuperior />
+        <BarraPesquisa />
+      </header>
+
+      <main className="container mx-auto py-6">
+        <h1 className="text-3xl">Rancho da Lua, tranquilo e rodeado por montanhas</h1>
+      </main>
+
+      <footer className="bg-gray-200">
+        <Rodape />
+      </footer>
+
+    </div>
+  );
 }
